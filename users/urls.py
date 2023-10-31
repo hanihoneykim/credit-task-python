65562e353c1ba0d44f9fe5a756964ae9e412cbb6
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-10-31 19:27:20 +0900

    Me(mypage) - get, put 작성

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.Users.as_view()),
+    path("me", views.Me.as_view()),
     path("me/projects", views.MyProjects.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
     path("change-password", views.ChangePassword.as_view()),
--- users/views.py
+++ users/views.py
@@ -96,3 +96,26 @@ class LogOut(APIView):
     def post(self, request):
         logout(request)
         return Response({"ok": "bye!"})
+
+
+class Me(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = PrivateUserSerializer(
+            user,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            user = serializer.save()
+            serializer = PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
